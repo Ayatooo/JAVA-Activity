d585d14f2750776332313ae905f8c85729d6b1bd
alexjnj1301 <114980961+alexjnj1301@users.noreply.github.com>
2023-06-13 14:48:55 +0200

    :zap: create 1 window

--- src/main/java/fr/ayato/activity/gui/Window.java
+++ src/main/java/fr/ayato/activity/gui/Window.java
@@ -1,4 +1,24 @@
 package fr.ayato.activity.gui;
 
-public class Window {
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+    public Window(){
+        super("Petite fenêtre bien sympa");
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        int screenHeightSize = tk.getScreenSize().height;
+        int screenWidthSize = tk.getScreenSize().width;
+
+        setBounds(0,0, screenWidthSize/2, screenHeightSize/2);
+        setLocationRelativeTo(null);
+        tk.beep();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new Window();
+    }
 }
