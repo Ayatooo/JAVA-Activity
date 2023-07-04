7e13ac9fe1abcf1174d5b47ca90ffadedf6e6617
Louis <72603854+Ayatooo@users.noreply.github.com>
2023-07-04 16:06:27 +0200

    :zap: activities order btn implemented

--- src/main/java/fr/ayato/activity/gui/WindowListActivities.java
+++ src/main/java/fr/ayato/activity/gui/WindowListActivities.java
@@ -12,6 +12,8 @@ import org.bson.Document;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import static fr.ayato.activity.mapper.ActivityMapper.documentToActivity;
@@ -23,6 +25,7 @@ public class WindowListActivities extends JFrame {
     ActivityControllerImpl activityController;
     ActivityRepositoryImpl activityRepository;
     MongoCollection<Document> collection;
+    boolean ascendingOrder = true;
 
     public WindowListActivities() {
         super("Liste des activités");
@@ -44,15 +47,21 @@ public class WindowListActivities extends JFrame {
 
         JButton refreshButton = new JButton("Actualiser");
         refreshButton.addActionListener(e -> refreshActivityList(textArea));
-        JButton addButton = new JButton("Ajouter");
-        addButton.addActionListener(e -> {
-            dispose(); // Ferme la fenêtre actuelle
-            new WindowCreateActivity(); // Ouvre la fenêtre de création d'activités
+
+        JButton sortButton = new JButton("Trier par date");
+        sortButton.addActionListener(e -> sortActivitiesByDate(textArea));
+
+        JButton addButton = new JButton("Créer");
+        addButton.addActionListener(e ->
+        {
+            dispose();
+        	new WindowCreateActivity();
         });
 
         JPanel buttonPanel = new JPanel(new FlowLayout());
-        buttonPanel.add(addButton);
         buttonPanel.add(refreshButton);
+        buttonPanel.add(sortButton);
+        buttonPanel.add(addButton);
         contentPane.add(buttonPanel, BorderLayout.SOUTH);
 
         refreshActivityList(textArea);
@@ -81,6 +90,39 @@ public class WindowListActivities extends JFrame {
                         + "Date : " + activityDTO.getDate() + "\n"
                         + "------------------------------------------------------\n"
         ));
+    }
+
+    private void sortActivitiesByDate(JTextArea textArea) {
+        this.collection = Connection.client(this.dotenv.get("DB_NAME"), this.dotenv.get("DB_COLLECTION_ACT"));
+        this.activityRepository = new ActivityRepositoryImpl(this.collection);
+        this.activityController = new ActivityControllerImpl(this.activityRepository);
+        MongoCollection<Document> activities = activityController.getAll();
+        List<ActivityDTO> activityDTOList = new ArrayList<>();
+        try {
+            for (Document activity : activities.find()) {
+                activityDTOList.add(documentToActivity(activity));
+            }
+        } catch (Exception e) {
+        }
+
+        // Trier par date
+        if (ascendingOrder) {
+            Collections.sort(activityDTOList, Comparator.comparing(ActivityDTO::getDate));
+        } else {
+            Collections.sort(activityDTOList, Comparator.comparing(ActivityDTO::getDate).reversed());
+        }
+
+        textArea.setText("");
+        activityDTOList.forEach(activityDTO -> textArea.append(
+                "Activité : " + activityDTO.getName() + "\n"
+                        + "Durée : " + activityDTO.getDuration() + "\n"
+                        + "RPE : " + activityDTO.getRpe() + "\n"
+                        + "Charge : " + activityDTO.getCharge() + "\n"
+                        + "Date : " + activityDTO.getDate() + "\n"
+                        + "------------------------------------------------------\n"
+        ));
+
+        ascendingOrder = !ascendingOrder; // Inverser l'ordre de tri
     }
 
     public static void main(String[] args) {
