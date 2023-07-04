5889f9e2ba52106ed39fcd773d15f7b9b5f660b5
Louis <72603854+Ayatooo@users.noreply.github.com>
2023-07-04 22:17:13 +0200

    :lipstick: design changed for HomeWindow

--- src/main/java/fr/ayato/activity/gui/HomeWindow.java
+++ src/main/java/fr/ayato/activity/gui/HomeWindow.java
@@ -1,16 +1,18 @@
 package fr.ayato.activity.gui;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicButtonUI;
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 
 public class HomeWindow extends JFrame {
-    public HomeWindow(){
+    public HomeWindow() {
         super("Fenêtre d'accueil");
         Toolkit tk = Toolkit.getDefaultToolkit();
         int screenHeightSize = tk.getScreenSize().height;
         int screenWidthSize = tk.getScreenSize().width;
 
-        setBounds(0,0, screenWidthSize/2, screenHeightSize/2);
+        setBounds(0, 0, screenWidthSize / 2, screenHeightSize / 2);
         setLocationRelativeTo(null);
 
         Container contentPane = getContentPane();
@@ -24,6 +26,25 @@ public class HomeWindow extends JFrame {
             new CreateUserWindow();
             dispose();
         });
+        buttonUser.setFocusPainted(false);
+        buttonUser.setBackground(new Color(63, 81, 181));
+        buttonUser.setForeground(Color.WHITE);
+        buttonUser.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonUser.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        buttonUser.setUI(new BasicButtonUI() {
+            @Override
+            public void paint(Graphics g, JComponent c) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                Shape shape = new RoundRectangle2D.Double(0, 0, c.getWidth(), c.getHeight(), 5, 5);
+                g2.setColor(c.getBackground());
+                g2.fill(shape);
+                g2.setColor(c.getForeground());
+                g2.draw(shape);
+                g2.dispose();
+                super.paint(g, c);
+            }
+        });
         contentPane.add(buttonUser, BorderLayout.NORTH);
 
         JButton buttonListActivities = new JButton("Lister les activités");
@@ -31,6 +52,25 @@ public class HomeWindow extends JFrame {
         buttonListActivities.addActionListener(e -> {
             new ActivityListWindow();
             dispose();
+        });
+        buttonListActivities.setFocusPainted(false);
+        buttonListActivities.setBackground(new Color(63, 81, 181));
+        buttonListActivities.setForeground(Color.WHITE);
+        buttonListActivities.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonListActivities.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        buttonListActivities.setUI(new BasicButtonUI() {
+            @Override
+            public void paint(Graphics g, JComponent c) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                Shape shape = new RoundRectangle2D.Double(0, 0, c.getWidth(), c.getHeight(), 5, 5);
+                g2.setColor(c.getBackground());
+                g2.fill(shape);
+                g2.setColor(c.getForeground());
+                g2.draw(shape);
+                g2.dispose();
+                super.paint(g, c);
+            }
         });
         contentPane.add(buttonListActivities, BorderLayout.CENTER);
 
@@ -40,10 +80,28 @@ public class HomeWindow extends JFrame {
             new CreateActivityWindow();
             dispose();
         });
+        buttonCreateActivity.setFocusPainted(false);
+        buttonCreateActivity.setBackground(new Color(63, 81, 181));
+        buttonCreateActivity.setForeground(Color.WHITE);
+        buttonCreateActivity.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonCreateActivity.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        buttonCreateActivity.setUI(new BasicButtonUI() {
+            @Override
+            public void paint(Graphics g, JComponent c) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                Shape shape = new RoundRectangle2D.Double(0, 0, c.getWidth(), c.getHeight(), 5, 5);
+                g2.setColor(c.getBackground());
+                g2.fill(shape);
+                g2.setColor(c.getForeground());
+                g2.draw(shape);
+                g2.dispose();
+                super.paint(g, c);
+            }
+        });
         contentPane.add(buttonCreateActivity, BorderLayout.SOUTH);
 
         pack();
         setVisible(true);
     }
-
 }
